c4de97d34880425bca7e7017c48710d36cdec3fb
Brian-SS <brian.sterling@correo.usa.edu.co>
2017-08-16 18:57:17 -0500

    03. Adaptacion

--- src/java/modelo/Saludo.java
+++ src/java/modelo/Saludo.java
@@ -1,7 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  TALLER ARCHIVOS ACCESO ALEATORIO
+ *  co-Author ::: Juan Albarracin
+ *  co-Author ::: Mario Bolaños
+ *  co-Author ::: Brian Sterling
+ *  Professor ::: Fabian Giraldo
+ *    Program ::: Bases de Datos
  */
 package modelo;
 
