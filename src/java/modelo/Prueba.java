bc57a495c35c9480d2b709bc6a458fd232250e41
Brian-SS <brian.sterling@correo.usa.edu.co>
2017-08-17 07:21:47 -0500

    05. Union Java - JSP

--- src/java/modelo/Prueba.java
+++ src/java/modelo/Prueba.java
@@ -8,43 +8,30 @@
  */
 package modelo;
 
-import java.io.FileNotFoundException;
+
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
-
-
-/**
- *
- * @author Mario-Bx
- */
-public class Prueba {
-
-    public static void main(String[] args) throws IOException {
-        ////Declaramos un objerto de la clase 
+public class Prueba
+{
+    public static void main(String[] args) throws IOException
+    {
+        //Declaramos un objerto de la clase 
         RandomAccessFile XFAleatorios;
-
-        String codigo = "1234";           ///4 CARACTERES
-        String nombre = "Mario Rojas";    ///11 CARACTERES
-        String edad = "29";               ///2 CARACTERES
-        String peso = "60.55";            ///5 CARACTERES
-        String activo = "*";              ///1 CARACTERES
-        String fol = "\r\n";              ///2 CARACTERES
+        String codigo = "1234";           //04 CARACTERES
+        String nombre = "Mario Rojas";    //11 CARACTERES
+        String edad = "29";               //02 CARACTERES
+        String peso = "60.55";            //05 CARACTERES
+        String activo = "*";              //01 CARACTERES
+        String fol = "\r\n";              //02 CARACTERES
         int longitud = 25;
         int registro;
-
         XFAleatorios = new RandomAccessFile("Datos.txt", "rw");
-
         System.out.println("longitud del archivo: " + XFAleatorios.length());
         System.out.println("longitud del registros: " + XFAleatorios.length() / longitud);
-
-//            XFAleatorios.seek(XFAleatorios.length());
         registro = 3;
         XFAleatorios.seek((registro - 1) * longitud);
-
         XFAleatorios.writeBytes(codigo);
         System.out.println(XFAleatorios.getFilePointer()); //Byte actual
         XFAleatorios.writeBytes(nombre);
@@ -57,9 +44,6 @@ public class Prueba {
         System.out.println(XFAleatorios.getFilePointer()); //Byte actual
         XFAleatorios.writeBytes(fol);
         System.out.println(XFAleatorios.getFilePointer()); //Byte actual
-
         XFAleatorios.close();
-
     }
-
 }
