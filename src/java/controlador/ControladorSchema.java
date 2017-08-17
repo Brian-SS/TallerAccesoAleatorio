db0a9db6093b6222acc7ca3dfa7ae76a12ffaf36
Brian-SS <brian.sterling@correo.usa.edu.co>
2017-08-17 08:49:35 -0500

    05. Union Java - JSP

--- src/java/controlador/ControladorSchema.java
+++ src/java/controlador/ControladorSchema.java
@@ -16,8 +16,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import modelo.Schema;
-import modelo.TArchivoAleatorioWeb;
 
 
 @WebServlet(name = "ControladorSchema", urlPatterns = {"/ControladorSchema"})
--- src/java/modelo/TArchivoAleatorioWeb.java
+++ src/java/modelo/TArchivoAleatorioWeb.java
@@ -21,51 +21,14 @@ public class TArchivoAleatorioWeb
     public static void main(String[] args) throws IOException  
     {
         // TODO code application logic here 21
-        
         Column cml= new Column();
         cml.crearArchivo();
-       
-                
-                
-
-        
-//        cml.crearColumn("String", "Elemento",1);
-//        cml.crearColumn("Int", "Telefono",1);
-//        cml.crearColumn("Boolean", "Soltero",4);
-//        cml.crearColumn("Float", "Peso",3);
-//        cml.crearColumn("Int", "C.C.",5);
-        
-//        cml.leerColum(2);
-//        cml.actualizarColum(1, "nombre", "Edad");
-//        cml.borrarRegistro(1);
-//        cml.existe(1);
-        
-        
-        
         Table tbl = new Table();
         tbl.crearArchivo();
-        
-//        tbl.crearTabla("Carros", 1);
-//        tbl.crearTabla("Deportes", 2);
-//        tbl.crearTabla("Materias", 3);
-//        tbl.crearTabla("Comidas", 4);
-//        tbl.crearTabla("Celulares", 5);
-        
-//        tbl.leerTabla(5);
-//        tbl.actualizarTabla(1, "nombre", "Cabello");
-//        tbl.borrarRegistro(2);
-        
         Schema sch = new Schema();
         sch.crearArchivo();
-        
-//        sch.crearSch("Prueba-1");
-//        sch.crearSch("Prueba-2");
-//        sch.crearSch("Prueba-3");
-        
         sch.leerSch(2);
         sch.actualizarSch(2, "Competencias");
-        sch.borrarRegistro(1);
-      
-    }
-    
+        sch.borrarRegistro(1);   
+    }  
 }
